f64c7a14e1b6552af4489b7ec85498f40f8dfb95
PRABIN1804 <prabinmishra2004@gmail.com>
2022-07-14 15:18:02 +0530

    bahubali

---
+++ C CODES/text.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2,sum;
+cout<<" enter two number";
+cin >> num1 >> num2;
+sum= num1 + num2;
+cout<< " sum of two number:" <<sum;
+return 0;
+} 
