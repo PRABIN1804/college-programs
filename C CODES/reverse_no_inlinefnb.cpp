4be10b4753353dcd76fc889988cc000f19172dd9
PRABIN1804 <prabinmishra2004@gmail.com>
2022-07-18 15:31:51 +0530

    files added

---
+++ C CODES/reverse_no_inlinefnb.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class item
+{
+	private:
+		int n,i;
+		public:
+	    int in();
+		int show();
+};
+	inline int item::in()
+		{
+			cout<<"Enter value to Display the number in reverse: ";
+			cin>>n;
+		}
+		inline int item::show()
+		{
+			cout<<"The reverse of the Entered Number: ";
+			for(i=n;n>0;n=n/10)
+			{
+				cout<<n%10;
+			}
+		}
+int main()
+{
+	item R;
+	R.in();
+    R.show();
+}
