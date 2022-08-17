d182c9159ebbd13db3a1d51d3c0a93136a20cb50
PRABIN1804 <prabinmishra2004@gmail.com>
2022-08-17 13:29:16 +0530

    files added

---
+++ C++ CODES/multi_level_inheritance34.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class worker
+{
+    public:
+    int age;
+    char name[20];
+    void get();
+    void show();
+};
+void worker::get()
+{
+    cout << " enter the name of the worker:"<<endl;
+    cin>>name;
+    cout << " enter the age of the worker:"<<endl;
+    cin>>age;
+}
+void worker::show()
+{
+    cout <<"the name of the worker is "<<name<< "and the is "<<age<<endl;
+}
+class manager : public worker
+{
+int num;
+public:
+void get();
+void show();
+};
+void manager::get()
+{
+    worker::get();
+    cout<<" enter the number of the worker:"<<endl;
+    cin>>num;
+}
+void manager::show(){
+    worker::show();
+    cout<< " the number of worker under me is:"<<num<<endl;
+}
+class ceo: public manager
