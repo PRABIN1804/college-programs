a93dc6aeee76802080fa4878d3bac073df056762
PRABIN1804 <prabinmishra2004@gmail.com>
2022-10-26 11:43:14 +0530

    files added

--- JAVA/Primeornot.java
+++ JAVA/Primeornot.java
@@ -1,4 +1,20 @@
 public class Primeornot 
 {
-    
+   public static void main(String[] args) {
+    int num= 3 ;
+    boolean flag = false;
+    for(int i =2; i<=num/2; ++i)
+    {
+        //condition for non prime number
+        if (num % 1==0)
+        {
+            flag = true;
+            break;
+        }
+    }
+    if (!flag)
+    System.out.println(num+ " is a prime number");
+    else
+    System.out.println(num+ " is not a prime number");
+   } 
 }
