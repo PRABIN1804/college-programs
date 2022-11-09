2c9a3ae4671a02c44c0fc6122e066ecf5f05207e
PRABIN1804 <prabinmishra2004@gmail.com>
2022-11-09 10:50:54 +0530

    files added

--- JAVA/swapping.java
+++ JAVA/swapping.java
@@ -1,4 +1,13 @@
 public class swapping 
  {
-    
+    // palincrome
+    //armstrong
+    //generate a number
+    //reverse a nymber
+    //convert a number to word
+    public static void main(String[] args) {
+        int x,y,t;
+        
+    }
+
 }
