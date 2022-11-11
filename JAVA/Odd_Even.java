ceb716d8e883faa78c06eebf0d3c405e4ed30d40
PRABIN1804 <prabinmishra2004@gmail.com>
2022-11-11 10:20:18 +0530

    files added

---
+++ JAVA/Odd_Even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Odd_Even 
+{
+    public static void main(String[] args){
+    Scanner read=new Scanner(System.in);
+
+    System.out.println("Enter a number:");
+    int num = read.nextInt();
+
+    if(num % 2==0)
+    System.out.println(num +" is even");
+    else 
+    System.out.println(num+" is odd");  
+    }
+}
