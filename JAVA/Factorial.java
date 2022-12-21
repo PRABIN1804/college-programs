39a928e58cdb1f43b049c31f4d543d5831fbfa9f
PRABIN1804 <prabinmishra2004@gmail.com>
2022-12-21 10:17:40 +0530

    files added

--- JAVA/Factorial.java
+++ JAVA/Factorial.java
@@ -6,15 +6,15 @@ public class Factorial {
     {
         double  factorial=1;
         double i;
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter a number:");
-        int num = sc.nextInt();
+        int num = in.nextInt();
         for(i = 1; i <= num; ++i)
         {
             factorial *=i;
         }
 
         System.out.println("factorial of " +num+ " is "  +factorial);
-        sc.close();
+        in.close();
     }
 }
