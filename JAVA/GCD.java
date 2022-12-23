53268234f0c56b30553081043d321e1b8e0df412
PRABIN1804 <prabinmishra2004@gmail.com>
2022-12-23 10:11:19 +0530

    files added

---
+++ JAVA/GCD.java
@@ -0,0 +1,16 @@
+public class GCD 
+  
+{  
+public static void main(String[] args)   
+{   
+int x = 12, y = 8, gcd = 1;   
+for(int i = 1; i <= x && i <= y; i++)  
+{  
+  
+if(x%i==0 && y%i==0)  
+ 
+gcd = i;  
+}    
+System.out.printf("GCD of %d and %d is: %d", x, y, gcd);  
+}  
+}  
