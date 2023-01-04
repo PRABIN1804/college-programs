f4c0a386d7279eb370a4cd8dd2de3175fed70add
PRABIN1804 <prabinmishra2004@gmail.com>
2023-01-04 11:10:22 +0530

    files added

--- JAVA/LeapYear.java
+++ JAVA/LeapYear.java
@@ -8,14 +8,14 @@ public class LeapYear {
     return false; 
   }
   public static void main(String[] args) {
-    Scanner in=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
     System.out.println("Enter a year:");
-    int year=in.nextInt();
+    int year=sc.nextInt();
 
     if (isLeapYear(year))
       System.out.println("This  is a leap year.");
     else
       System.out.println( "This is not a leap year.");
-      in.close();
+      sc.close();
   }
 }
